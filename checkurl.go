b7dbd9926f31ef131de9d43243f8850d5b58a2bc
Oliver Hough <4314648+olihough86@users.noreply.github.com>
2023-04-23 20:11:21 +0200

    imgur changes, proxy, update README

--- checkurl.go
+++ checkurl.go
@@ -8,33 +8,34 @@ import (
 )
 
 func checkURL(baseURL, urlPath, ext string, wg *sync.WaitGroup, sem chan struct{}, validURLs chan<- string, totalRequests *int64, treat301AsValid, noExt bool, client *http.Client) {
-	defer wg.Done()
-	
-	sem <- struct{}{} // Acquire semaphore
-	defer func() { <-sem }()
-	
-	fullURL := baseURL + urlPath
-	if !noExt {
-		fullURL += ext
-	}
-	
-	req, err := http.NewRequest("HEAD", fullURL, nil)
-	if err != nil {
-		fmt.Printf("Error creating request for %s: %v\n", fullURL, err)
-		return
-	}
-	
-	resp, err := client.Do(req)
-	if err != nil {
-		fmt.Printf("Error while accessing %s: %v\n", fullURL, err)
-		return
-	}
-	defer resp.Body.Close()
-	
-	if resp.StatusCode == 200 || (treat301AsValid && resp.StatusCode == 301) {
-		fmt.Println("Valid:", fullURL) // Print the valid URL
-		validURLs <- fullURL
-	}
-	
-	atomic.AddInt64(totalRequests, 1)
+    defer wg.Done()
+
+    sem <- struct{}{} // Acquire semaphore
+    defer func() { <-sem }()
+
+    fullURL := baseURL + urlPath
+    if !noExt {
+        fullURL += ext
+    }
+
+    req, err := http.NewRequest("HEAD", fullURL, nil)
+    if err != nil {
+        fmt.Printf("Error creating request for %s: %v\n", fullURL, err)
+        return
+    }
+
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Printf("Error while accessing %s: %v\n", fullURL, err)
+        return
+    }
+    defer resp.Body.Close()
+
+    // Check for 200 status code and not redirected to removed.png
+    if resp.StatusCode == 200 && resp.Request.URL.String() != "https://i.imgur.com/removed.png" {
+        fmt.Println("Valid:", fullURL) // Print the valid URL
+        validURLs <- fullURL
+    }
+
+    atomic.AddInt64(totalRequests, 1)
 }
